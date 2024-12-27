ee992b39d0207d5632a92907f7be9d682cc8a960
Michaël Bourjac <michael.bourjac@gmail.com>
2024-12-27 18:23:39 +0100

    fix: rename pathless scenes route

--- src/routes/_scenes.tsx
+++ src/routes/_scenes.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { ScenesLayout } from '../layouts/ScenesLayout';
 
-export const Route = createFileRoute('/_scene')({
+export const Route = createFileRoute('/_scenes')({
   component: ScenesLayout,
 });
