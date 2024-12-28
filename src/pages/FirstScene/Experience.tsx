e1b9c268412a047d052f27fa6ced88106ccfcc91
Michaël Bourjac <michael.bourjac@gmail.com>
2024-12-28 08:20:56 +0100

    feat: arotate cube

--- src/pages/FirstScene/Experience.tsx
+++ src/pages/FirstScene/Experience.tsx
@@ -1,4 +1,18 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import type * as THREE from 'three';
+
 export const Experience = () => {
+  const cubeRef = useRef<THREE.Mesh | null>(null);
+
+  useFrame((_state, delta) => {
+    const cube = cubeRef.current;
+
+    if (!cube) return;
+
+    cube.rotation.y += delta;
+  });
+
   return (
     <>
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
@@ -9,7 +23,12 @@ export const Experience = () => {
         <sphereGeometry />
         <meshBasicMaterial color="orange" />
       </mesh>
-      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+      <mesh
+        ref={cubeRef}
+        rotation-y={Math.PI * 0.25}
+        position-x={2}
+        scale={1.5}
+      >
         <boxGeometry />
         <meshBasicMaterial color="mediumpurple" />
       </mesh>
