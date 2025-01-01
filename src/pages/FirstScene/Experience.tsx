f364c98ddfa5488c2e448a64d9678ae1a4e02a62
Michaël Bourjac <michael.bourjac@gmail.com>
2025-01-01 18:33:16 +0100

    feat: animate objects group

--- src/pages/FirstScene/Experience.tsx
+++ src/pages/FirstScene/Experience.tsx
@@ -3,13 +3,16 @@ import { useFrame } from '@react-three/fiber';
 import type * as THREE from 'three';
 
 export const Experience = () => {
+  const groupRef = useRef<THREE.Group | null>(null);
   const cubeRef = useRef<THREE.Mesh | null>(null);
 
   useFrame((_state, delta) => {
+    const group = groupRef.current;
     const cube = cubeRef.current;
 
-    if (!cube) return;
+    if (!group || !cube) return;
 
+    group.rotation.y += delta;
     cube.rotation.y += delta;
   });
 
@@ -19,19 +22,21 @@ export const Experience = () => {
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
       </mesh>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
-      </mesh>
-      <mesh
-        ref={cubeRef}
-        rotation-y={Math.PI * 0.25}
-        position-x={2}
-        scale={1.5}
-      >
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+        </mesh>
+        <mesh
+          ref={cubeRef}
+          rotation-y={Math.PI * 0.25}
+          position-x={2}
+          scale={1.5}
+        >
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+      </group>
     </>
   );
 };
