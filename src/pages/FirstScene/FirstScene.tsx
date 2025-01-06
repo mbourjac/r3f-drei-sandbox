88a419d3410cce0808f5f45402f001f01c276446
Michaël Bourjac <michael.bourjac@gmail.com>
2025-01-06 08:12:20 +0100

    feat: update camera settings and position

--- src/pages/FirstScene/FirstScene.tsx
+++ src/pages/FirstScene/FirstScene.tsx
@@ -3,7 +3,7 @@ import { Experience } from './Experience';
 
 export const FirstScene = () => {
   return (
-    <Canvas>
+    <Canvas camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}>
       <Experience />
     </Canvas>
   );
