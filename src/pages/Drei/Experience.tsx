7e9d5c5f2fbafe2e12175129805a5f9a7e00e686
Michaël Bourjac <michael.bourjac@gmail.com>
2025-01-09 08:46:11 +0100

    feat: use transform controls

--- src/pages/Drei/Experience.tsx
+++ src/pages/Drei/Experience.tsx
@@ -1,9 +1,13 @@
-import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
+import { OrbitControls, TransformControls } from '@react-three/drei';
+import type { Mesh } from 'three';
 
 export const Experience = () => {
+  const cubeRef = useRef<Mesh>(null!);
+
   return (
     <>
-      <OrbitControls />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
@@ -13,10 +17,11 @@ export const Experience = () => {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh ref={cubeRef} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+      <TransformControls object={cubeRef} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
