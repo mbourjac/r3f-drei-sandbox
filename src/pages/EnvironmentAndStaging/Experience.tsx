f06b3e1f2c901462766e07ff9493d941535a1058
Michaël Bourjac <michael.bourjac@gmail.com>
2025-10-01 19:31:37 +0200

    feat(environment-and-staging): use stage component

--- src/pages/EnvironmentAndStaging/Experience.tsx
+++ src/pages/EnvironmentAndStaging/Experience.tsx
@@ -1,11 +1,6 @@
-import { useEffect, useRef } from 'react';
-import {
-  ContactShadows,
-  Environment,
-  OrbitControls,
-  useHelper,
-} from '@react-three/drei';
-import { useFrame, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls, Stage, useHelper } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import { useControls } from 'leva';
 import { Perf } from 'r3f-perf';
 import * as THREE from 'three';
@@ -22,25 +17,9 @@ export const Experience = () => {
     cubeRef.current.rotation.y += delta * 0.2;
   });
 
-  const { color, opacity, blur } = useControls('contact shadows', {
-    color: '#1d8f75',
-    opacity: { value: 0.4, min: 0, max: 1 },
-    blur: { value: 2.8, min: 0, max: 10 },
+  const { envMapIntensity } = useControls('environment map', {
+    envMapIntensity: { value: 7, min: 0, max: 12 },
   });
-
-  const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } =
-    useControls('environment map', {
-      envMapIntensity: { value: 7, min: 0, max: 12 },
-      envMapHeight: { value: 7, min: 0, max: 100 },
-      envMapRadius: { value: 28, min: 10, max: 1000 },
-      envMapScale: { value: 100, min: 10, max: 1000 },
-    });
-
-  const scene = useThree((state) => state.scene);
-
-  useEffect(() => {
-    scene.environmentIntensity = envMapIntensity;
-  }, [scene, envMapIntensity]);
 
   return (
     <>
@@ -48,36 +27,30 @@ export const Experience = () => {
 
       <OrbitControls makeDefault />
 
-      <Environment
-        preset="sunset"
-        ground={{
-          height: envMapHeight,
-          radius: envMapRadius,
-          scale: envMapScale,
-        }}
-      ></Environment>
+      <color args={['ivory']} attach="background" />
 
-      <ContactShadows
-        position={[0, 0, 0]}
-        scale={10}
-        resolution={512}
-        far={5}
-        color={color}
-        opacity={opacity}
-        blur={blur}
-      />
+      <Stage
+        shadows={{ type: 'contact', opacity: 0.2, blur: 3 }}
+        environment="sunset"
+        preset="portrait"
+        intensity={envMapIntensity}
+      >
+        <mesh position-y={1} position-x={-2} castShadow>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
 
-      <color args={['#000000']} attach="background" />
-
-      <mesh position-y={1} position-x={-2} castShadow>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-
-      <mesh ref={cubeRef} position-x={2} position-y={1} scale={1.5} castShadow>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+        <mesh
+          ref={cubeRef}
+          position-x={2}
+          position-y={1}
+          scale={1.5}
+          castShadow
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+      </Stage>
     </>
   );
 };
