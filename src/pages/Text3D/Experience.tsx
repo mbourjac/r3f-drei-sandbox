f6d8bd1b470149d1c43bf1fefe5c83b3fa3a843f
Michaël Bourjac <michael.bourjac@gmail.com>
2025-10-09 20:08:41 +0200

    feat(3d-text): add attributes to text 3d component

--- src/pages/Text3D/Experience.tsx
+++ src/pages/Text3D/Experience.tsx
@@ -11,7 +11,17 @@ export const Experience = () => {
       <color args={['ivory']} attach="background" />
 
       <Center>
-        <Text3D font="./fonts/helvetiker_regular.typeface.json">
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
           LOREM IPSUM
           <meshNormalMaterial />
         </Text3D>
