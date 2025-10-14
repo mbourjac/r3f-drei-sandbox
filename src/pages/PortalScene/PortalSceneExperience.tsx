82c10b8fa15456419acb081996c0c41d2968540d
Michaël Bourjac <michael.bourjac@gmail.com>
2025-10-14 20:09:21 +0200

    feat(portal-scene): update background color

--- src/pages/PortalScene/PortalSceneExperience.tsx
+++ src/pages/PortalScene/PortalSceneExperience.tsx
@@ -3,6 +3,7 @@ import { OrbitControls } from '@react-three/drei';
 export const PortalSceneExperience = () => {
   return (
     <>
+      <color args={['#030202']} attach="background" />
       <OrbitControls makeDefault />
       <mesh scale={1.5}>
         <boxGeometry />
