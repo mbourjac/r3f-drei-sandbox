60a14452706a49e184a5318aebe483cda5f197e4
Michaël Bourjac <michael.bourjac@gmail.com>
2025-10-28 19:52:30 +0100

    feat(post-processing): add glitch effect

--- src/pages/PostProcessing/Experience.tsx
+++ src/pages/PostProcessing/Experience.tsx
@@ -1,16 +1,39 @@
 import { OrbitControls } from '@react-three/drei';
 import {
   EffectComposer,
+  Glitch,
   ToneMapping,
   Vignette,
 } from '@react-three/postprocessing';
 import { useControls } from 'leva';
-import { BlendFunction, ToneMappingMode } from 'postprocessing';
+import { BlendFunction, GlitchMode, ToneMappingMode } from 'postprocessing';
 import { Perf } from 'r3f-perf';
+import * as THREE from 'three';
 
 export const Experience = () => {
-  const { blendFunction } = useControls('vignette', {
+  const { blendFunction, enabled: enabledVignette } = useControls('vignette', {
+    enabled: { value: false },
     blendFunction: { options: BlendFunction },
+  });
+
+  const {
+    enabled: enabledGlitch,
+    glitchMode,
+    delayMin,
+    delayMax,
+    durationMin,
+    durationMax,
+    strengthMin,
+    strengthMax,
+  } = useControls('glitch', {
+    enabled: { value: false },
+    glitchMode: { options: GlitchMode },
+    delayMin: { value: 0.5, min: 0, max: 5, step: 0.1 },
+    delayMax: { value: 1, min: 0, max: 5, step: 0.1 },
+    durationMin: { value: 0.1, min: 0, max: 5, step: 0.1 },
+    durationMax: { value: 0.3, min: 0, max: 5, step: 0.1 },
+    strengthMin: { value: 0.2, min: 0, max: 1, step: 0.01 },
+    strengthMax: { value: 0.4, min: 0, max: 1, step: 0.01 },
   });
 
   return (
@@ -18,7 +41,17 @@ export const Experience = () => {
       <Perf position="top-left" />
 
       <EffectComposer>
-        <Vignette offset={0.3} darkness={0.9} blendFunction={blendFunction} />
+        {enabledVignette ?
+          <Vignette offset={0.3} darkness={0.9} blendFunction={blendFunction} />
+        : <></>}
+        {enabledGlitch ?
+          <Glitch
+            delay={new THREE.Vector2(delayMin, delayMax)}
+            duration={new THREE.Vector2(durationMin, durationMax)}
+            strength={new THREE.Vector2(strengthMin, strengthMax)}
+            mode={glitchMode as GlitchMode}
+          />
+        : <></>}
         <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
       </EffectComposer>
 
